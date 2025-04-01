2b5a9654351c844583a53310581be57de7e73001
alexv26 <velsmida@bc.edu>
2025-04-01 17:09:51 -0400

    Added example of how to use word2vec model

---
+++ data_processing/word2vec/using_word2vec_model_example.py
@@ -0,0 +1,11 @@
+from gensim.models import Word2Vec
+
+# Load the saved Word2Vec model
+word2vec_model = Word2Vec.load("word2vec_code.model")
+
+# Test: Find similar words
+print(word2vec_model.wv.most_similar("ssl"))  # Example: Words similar to 'ssl'
+
+# Get vector representation of a word
+vector = word2vec_model.wv["return"]
+print(vector)
